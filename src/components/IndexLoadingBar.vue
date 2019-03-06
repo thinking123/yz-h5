448d2dda5b546104e0e7951f17021b52bd10dae4
TK <129487004@qq.com>
2019-03-07 07:45:52 +0800

    bugfix

--- src/components/IndexLoadingBar.vue
+++ src/components/IndexLoadingBar.vue
@@ -70,7 +70,7 @@
             left: 50%;
             top: 50%;
             transform: translate(-50%,-50%);
-            font-size: 30px;
+            font-size: 13px;
             color: #e34981;
             font-weight: bolder;
         }
